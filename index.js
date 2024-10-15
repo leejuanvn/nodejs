7dfdfb02af4d5003ae91c1713e01115813772816
leejuanvn <102353189+leejuanvn@users.noreply.github.com>
2024-10-15 10:15:17 +0700

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Sử dụng biến môi trường PORT
 
 // Middleware để nhận JSON
 app.use(express.json());
